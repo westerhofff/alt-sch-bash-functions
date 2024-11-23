ea567515f749e9ddbb0529281aad483d06eba342
dapobelieve <dapomichaels@gmail.com>
2024-11-23 19:26:41 +0100

    learning about functions in bash

--- functions.sh
+++ functions.sh
@@ -1,1 +1,32 @@
-# bash functions
+#!/bin/bash
+kitchen() {
+  echo "Hello I am in the kitchen cooking"
+  pantry
+}
+
+bedroom() {
+  echo "Hello from [Bedroom] I am getting dressed"
+}
+
+pantry() {
+  echo "Hello from [Pantry] I am storing foods"
+}
+
+greet_user() {
+  echo "Hello, $1! Welcome to Bash Scripting."
+}
+
+greet_user "Olabisi"
+
+
+number_doubler() {
+  local result=$(($1 * 2))
+  return $result
+}
+
+number_doubler 3
+
+echo "$?"
+
+# Global scope cannot access the Local scope
+# Local scope can access the Global scope
