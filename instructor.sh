eea7e4594eef02335a36e22af00cc6901bad3580
dapobelieve <dapomichaels@gmail.com>
2024-11-23 20:07:31 +0100

    git assignment

---
+++ instructor.sh
@@ -0,0 +1,11 @@
+who_am_i() {
+  echo "Hello, I am the Cloud Instructo🎉"
+}
+
+# write bash fucntions that prints out the following infomation about you
+
+# Fullname
+# Bio
+# Year in tech
+# Dream job
+# Goals ⚽
