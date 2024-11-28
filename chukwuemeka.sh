a2259408c05ffb5cd34eab8a5b5f862c46a0e51d
emka-sudo <ezeliorae@gmail.com>
2024-11-28 09:15:02 +0100

    First Assignment

---
+++ chukwuemeka.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+who_am_i() {
+    echo "Hello, I am Chukwuemeka Ezeliora Abiodun"
+}
+
+Bio() { 
+    echo "I am in the cloud engineering track with reg no: ALT/SOE/024/1175"
+    echo "I am 23years old, i'm currently a student studying comuputer science in the university"
+    echo "I school in enugu but live in Lagos"
+    echo "I am a crypto enthusiast"
+    echo "I love playing games" 
+}
+
+Years_in_tech() {
+    echo "I've been in tech for one year" 
+}
+
+Dream_job() {
+    echo "I would like to work as a DevOps engineer at Nvidia" 
+}
+
+Goals() {
+    echo "I would love to become a professional in my field"
+    echo "To incoprate web3 into my tech life"
+    echo "To develop tech solutions that would help the country"
+    echo "To improve on my programming languages" 
+}
+
+who_am_i 
+echo
+
+Bio 
+echo
+
+Years_in_tech 
+echo
+
+Dream_job 
+echo
+
+Goals 
+
