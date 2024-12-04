e5c59cb6681e99a49ef1b2e3aa52b2cae30d2181
westerhofff <sammiederahnnamani@gmail.com>
2024-12-04 01:31:14 +0100

    chideras assignment

---
+++ chidera.sh
@@ -0,0 +1,18 @@
+who_am_i() {
+    echo "Hello, my name is Chidera Nnamani"
+}
+
+Bio() {
+    echo "I am a passionate tech enthusiast, currently a learner at ALTschool as a cloud engineering student with Student ID: ALT/SOE/024/0086, with hands-on experience gained through internships in DevOps"
+}
+Year_in_tech() {
+    echo "I have been in tech especially cloud technologies for a little over 1 year now"
+}
+Dream_job() {
+    echo "I want to be a highly skilled professional in DevOps,so i can run my own tech company someday"
+}
+Goals() {
+    echo "I am an avid learner,so i want to learn learn learn everything there is to know about cloud engineering"
+    echo "I want to run my own tech company where we revolutionize the tech landscape"
+    echo "I want to look at myself 5 years from now and be proud of how far i have come"
+    echo "I want to land a great DevOps job before the end of my cloud engineering diploma at ALTschool Africa"
